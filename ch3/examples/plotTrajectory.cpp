a3e00d667a7ed57a82d3a505cd80e5ff5b34d565
miaoty <1761641864@qq.com>
2022-03-12 10:56:32 +0800

    0311

--- ch3/examples/plotTrajectory.cpp
+++ ch3/examples/plotTrajectory.cpp
@@ -8,7 +8,7 @@ using namespace std;
 using namespace Eigen;
 
 // path to trajectory file
-string trajectory_file = "./examples/trajectory.txt";
+string trajectory_file = "/home/tymiao2/slambook2/slambook2/ch3/examples/trajectory.txt";
 
 void DrawTrajectory(vector<Isometry3d, Eigen::aligned_allocator<Isometry3d>>);
 
