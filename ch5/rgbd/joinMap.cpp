75dab7ad08568010e56ac0cf4c40570c7bfc4d1e
miaoty <1761641864@qq.com>
2022-03-15 14:28:33 +0800

    tymiao

--- ch5/rgbd/joinMap.cpp
+++ ch5/rgbd/joinMap.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
     vector<cv::Mat> colorImgs, depthImgs;    // 彩色图和深度图
     TrajectoryType poses;         // 相机位姿
 
-    ifstream fin("./pose.txt");
+    ifstream fin("../pose.txt");
     if (!fin) {
         cerr << "请在有pose.txt的目录下运行此程序" << endl;
         return 1;
@@ -25,8 +25,8 @@ int main(int argc, char **argv) {
 
     for (int i = 0; i < 5; i++) {
         boost::format fmt("./%s/%d.%s"); //图像文件格式
-        colorImgs.push_back(cv::imread((fmt % "color" % (i + 1) % "png").str()));
-        depthImgs.push_back(cv::imread((fmt % "depth" % (i + 1) % "pgm").str(), -1)); // 使用-1读取原始图像
+        colorImgs.push_back(cv::imread((fmt % "../color" % (i + 1) % "png").str()));
+        depthImgs.push_back(cv::imread((fmt % "../depth" % (i + 1) % "pgm").str(), -1)); // 使用-1读取原始图像
 
         double data[7] = {0};
         for (auto &d:data)
